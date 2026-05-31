67470880d09386b1d8df7906408b7ad0778f2a7d
katsuqkv <sululolacaleb@gmail.com>
2026-05-31 14:02:30 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "All-in-one Discord Bot.",
+        name: "Bot For The Unity Lounge Server",
         // Activity type number (0 = Playing).
         type: 2, 
       },
